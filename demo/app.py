a10472e1254d6a10a5cb74cd8701c8dc287a6a27
Joachim Lusiardi <jol@solute.de>
2020-05-18 09:52:05 +0200

    Solve problem for relative files
    Solve the issue thanks to https://www.komoot.de/tour/185395400

--- demo/app.py
+++ demo/app.py
@@ -1,10 +1,19 @@
 import connexion
+import prance
 from flask_cors import CORS
+from typing import Any, Dict
+from pathlib import Path
+
+def get_bundled_specs(main_file: Path) -> Dict[str, Any]:
+    parser = prance.ResolvingParser(str(main_file.absolute()),
+                                    lazy = True, strict = True)
+    parser.parse()
+    return parser.specification
 
 app = connexion.App(__name__, specification_dir='swagger/')
 CORS(app.app)
 
-app.add_api('api.yaml')
+app.add_api(get_bundled_specs(Path('demo/swagger/api.yaml')), resolver = connexion.RestyResolver("cms_rest_api")) 
 
 
 if __name__ == '__main__':
